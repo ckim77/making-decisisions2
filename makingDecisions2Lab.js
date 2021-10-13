c8978ec5ee30d1567f53fe61c7890ef381298f75
Chris Kim <ckjr92@gmail.com>
2021-10-13 13:09:45 -0600

    Finished all questions

--- makingDecisions2Lab.js
+++ makingDecisions2Lab.js
@@ -56,14 +56,25 @@ for (i in nums) {
 ////////// PROBLEM 5 //////////
 
 // Do not edit the code below.
-var score = 74
+var score = 90
 // Do not edit the code above.
 
 /*
   Determine if the letter grade of the given variable 'score'. If the variable is a 90 or above, console log an 'A', between 80 and 89, console log a 'B', between 70 and 79, 'C', between 60 and 69, 'D', and anything below 60 should console log an 'F'.
 */
 
-//Code Here
+// if (score >= 90) {
+//   console.log('A');
+// } else if (score >= 80 && score < 90) {
+//   console.log('B');
+// } else if (score >= 70 && score < 80) {
+//   console.log('C')
+// } else if (score >= 60 && score < 70) {
+//   console.log('D')
+// } else {
+//   console.log('F')
+// }
+ 
 
 
 ////////// Intermediate Problems //////////
@@ -78,7 +89,8 @@ var myFavoriteNumbers = [4,8,12,16,20,24];
   Create a variable named 'someNum' and set it equal to the fifth value of the 'myFavoriteNumbers' array.
 */
 
-//Code Here
+// var someNum = myFavoriteNumbers[4]
+// console.log(someNum)
 
 
 ////////// PROBLEM 7 //////////
@@ -86,7 +98,13 @@ var myFavoriteNumbers = [4,8,12,16,20,24];
 // Subscripting (accessing values using their index) an array can fail. What happens if you subscript to the 7th element, but there are not 7 elements in the array? Let's write some code to check for that. 
 // Use an if statement to check the length of the 'myFavoriteNumbers' array. If it's less than 7, log 'There are not enough elements in this array' to the console. If the length is more than 7, reassign the value of 'someNum' to the value of the 7th element in the array. (Hint: how can you make sure that your code works for exactly 7 elements? What index do you use to get the 7th element?)
 
-//Code Here
+// var someNum = 0
+
+// if (myFavoriteNumbers.length < 7) {
+//   console.log('There are not enough elements in this array');
+// } else {
+//   someNum = myFavoriteNumbers[6];
+// }
 
 
 ////////// PROBLEM 8 //////////
@@ -97,7 +115,11 @@ var listOfNumbers = [1,2,3,4,5,6,7,8,9,10,11,12];
 
 // Use a for-loop to iterate through 'listOfNumbers', checking to see if each number is divisible by 3. If it is, console.log '{number} is divisible by 3.'
 
-//Code Here
+// for (i in listOfNumbers) {
+//   if (listOfNumbers[i] % 3 === 0) {
+//     console.log (`${listOfNumbers[i]} is divisible by 3`)
+//   }
+// }
 
 
 ////////// PROBLEM 9 //////////
@@ -109,7 +131,10 @@ var letters = ['A', 'B', 'C', 'D', 'E'];
   Loop backwards, starting at the end of the 'letters' array. Console log every item in the array.
 */
 
-//Code Here
+// for (var i = letters.length - 1; i >= 0; i--) {
+//   console.log(letters[i])
+// }
+
 
 
 ////////// Advanced Problems //////////
@@ -131,8 +156,25 @@ If F: "The student is failing."
 If the letter grade is not one of the above letters, console.log 'Not an eligible grade.'
 */
 
-//Code Here
-
+// switch (letterGrade) {
+//   case 'A':
+//     console.log('The student is doing excellently.');
+//     break;
+//   case 'B':
+//     console.log ('The student is doing well.');
+//     break;
+//   case 'C':
+//     console.log ('The student is doing alright.');
+//     break;
+//   case 'D':
+//     console.log ('The student is not doing very well.');
+//     break;
+//   case 'F':
+//     console.log ('The student is failing.');
+//     break;
+//   default:
+//     console.log ('Not an eligible grade.')
+// }
 
 
 ////////// PROBLEM 11 //////////
@@ -159,4 +201,15 @@ If the letter grade is not one of the above letters, console.log 'Not an eligibl
   ...
 */
 
-//Code Here
+// for (let i = 1; i <= 100; i ++) {
+//   if (i % 3 === 0 && i % 5 !== 0) {
+//     console.log('Dev');
+//   } else if (i % 3 !== 0 && i % 5 ===0) {
+//     console.log('mountain');
+//   } else if (i % 3 === 0 && i % 5 === 0) {
+//     console.log('Devmountain');
+//   } else {
+//     console.log(i)
+//   }
+// }
+
